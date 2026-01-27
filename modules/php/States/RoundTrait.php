2b26969b6c05e70af403ddd5e8081eabc9d4c9ce
Syarwin <tim.pecatte@gmail.com>
2026-01-27 11:51:16 +0100

    Revert the change on adrenaline

--- modules/php/States/RoundTrait.php
+++ modules/php/States/RoundTrait.php
@@ -844,7 +844,7 @@ trait RoundTrait
     $event = Globals::getCurrentEvent();
 
     // Adrenaline
-    $no = $constructor->getNo() + count(Globals::getFinishedConstructors()) + count(Globals::getSkippedPlayers());
+    $no = $constructor->getNo() + count(Globals::getFinishedConstructors());
     $maxNo = Constructors::count();
     if ($event != EVENT_SUDDEN_RAIN && ($no == $maxNo - 1 || ($no == $maxNo - 2 && $maxNo >= 5))) {
       $symbols[COOLDOWN]['entries'][ADRENALINE] = [
